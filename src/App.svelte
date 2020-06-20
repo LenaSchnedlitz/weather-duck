a1120c4a239756b2d2b1e11a8bac073ae3a45300
Lena Schnedlitz <git.lena.schnedlitz@web.de>
2020-06-20 19:53:08 +0200

    Import svelte template

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<main>
+  <h1>Weather Duck</h1>
+  <form method="get" id="ddgSearch" action="https://duckduckgo.com/">
+    <input type="text" name="q" placeholder="Search…" aria-label="Search on DuckDuckGo"/>
+  </form>
+</main>
